7e5e43019412527e0d86e90a911f7ad071f16be3
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-12-03 02:56:24 -0300

    Project has his basic startup files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # serradostoledosAPIs
 These project was made to serve serradostoledos.org frontend app
+
+### Commands to remember:
+
+`heroku login`
+`git push heroku main`
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+
+const importData = require("./data.json");
 let port  = process.env.PORT || 3000;
 
 app.get("/", (req,res) => {
     res.send("Hello Borges!!")
+})
+
+app.get("/players", (req,res) => {
+    res.send(importData);
 })
 
 app.listen(port, () => {
